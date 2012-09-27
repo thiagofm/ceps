46e1ba2fcf415552f812e4456439e159c3cc6d5c
Thiago Fernandes Massa <thiagown@gmail.com>
2012-09-27 17:50:37 -0300

    Changed to RESTful API in order to not have to deal with jsonp.
    Changed to RESTful API in order to not have to deal with jsonp.

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ http.use(express.logger());
 db = new pg.Client(process.env.DATABASE_URL);
 db.connect();
 
-http.get('/', function(request, response) {
+http.get('/cep/:cep_number', function(request, response) {
   var q;
 
   q = 'SELECT LG."CEP", LB."BAI_NO", LC."LOC_NO", LG."UFE_SG", LG."TLO_TX", LG."LOG_NO" FROM "LOG_LOGRADOURO" LG LEFT JOIN "LOG_LOCALIDADE" LC ON LC."LOC_NU" = LG."LOC_NU" LEFT JOIN "LOG_BAIRRO" LB ON LG."BAI_NU_INI" = LB."BAI_NU" WHERE LG."CEP" = $1 LIMIT 1;';
 
-  db.query(q, [request.query.CEP], function(error, result) {
+  db.query(q, [request.params.cep_number], function(error, result) {
     response.json(error || result.rows);
   });
 });
